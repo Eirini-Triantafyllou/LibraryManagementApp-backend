5bdcee3fe09c48653654fdea9ebd51093e6a1d6d
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-12-14 21:07:28 +0200

    Add mapper configuration

---
+++ LibraryManagementApp/Configuration/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LibraryManagementApp.Data;
+using LibraryManagementApp.DTO;
+
+namespace LibraryManagementApp.Configuration
+{
+    public class MapperConfig : Profile
+    {
+
+        public MapperConfig()
+        {
+            CreateMap<User, UserReadOnlyDTO>().ReverseMap();
+        }
+    }
+}
