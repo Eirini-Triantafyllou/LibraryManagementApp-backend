b4223bf76050a536ef7517252c16916862d2c2b6
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-12-19 18:26:44 +0200

    Add Encryption Util

---
+++ LibraryManagementApp/Security/EncryptionUtil.cs
@@ -0,0 +1,17 @@
+﻿namespace LibraryManagementApp.Security
+{
+    public static class EncryptionUtil
+    {
+
+        public static string Encrypt(string plaintext)
+        {
+            var encryptedPassword = BCrypt.Net.BCrypt.HashPassword(plaintext);
+            return encryptedPassword;
+        }
+
+        public static bool IsValidPassword(string plaintext, string cipherText)
+        {
+            return BCrypt.Net.BCrypt.Verify(plaintext, cipherText);
+        }
+    }
+}
