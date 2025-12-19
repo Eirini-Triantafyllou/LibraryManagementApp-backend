210d0f1cc18a624d5e930aa691f0306250d5b4e9
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-12-19 18:29:35 +0200

    Add UserFiltersDTO

---
+++ LibraryManagementApp/Core/Filters/UserFiltersDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagementApp.Core.Filters
+{
+    public class UserFiltersDTO
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? UserRole { get; set; }
+
+    }
+}
