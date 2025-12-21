f9c30f705e02654edba4bde8e8d02ab6f11214be
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-12-21 23:25:50 +0200

    Add UpdateUserReader DTO

---
+++ LibraryManagementApp/DTO/UpdateUserReaderDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagementApp.DTO
+{
+    public class UpdateUserReaderDTO
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+       
+    }
+}
