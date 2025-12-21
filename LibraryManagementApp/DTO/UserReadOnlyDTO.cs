f6ed2757dddd21f7e09a8f4edec237fab8f86355
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-12-21 17:40:34 +0200

    Add UserReadOnly DTO

---
+++ LibraryManagementApp/DTO/UserReadOnlyDTO.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementApp.Core.Enums;
+
+namespace LibraryManagementApp.DTO
+{
+    public record UserReadOnlyDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+        public string UserRole { get; set; } = null!;
+
+    }
+}
