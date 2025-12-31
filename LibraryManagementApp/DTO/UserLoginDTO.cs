35dd955a3dd790d22b55d06f834eedace54ccaf5
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-12-31 20:52:32 +0200

    fix: update user login DTO

--- LibraryManagementApp/DTO/UserLoginDTO.cs
+++ LibraryManagementApp/DTO/UserLoginDTO.cs
@@ -4,8 +4,8 @@ namespace LibraryManagementApp.DTO
 {
     public record UserLoginDTO
     {
-        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
-        public string? Username { get; set; }
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Email must be between 2 and 50 characters.")]
+        public string? Email { get; set; }
 
         [RegularExpression(@"(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?\W)^.{8,}$",
             ErrorMessage = "Password must contain at least one uppercase, one lowercase, " +
