81aa91df57e42950b45a454cdf931be137dae54d
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-02 10:02:27 +0200

    fix: update UserLoginDTO and UserReadOnlyDTO

--- LibraryManagementApp/DTO/UserReadOnlyDTO.cs
+++ LibraryManagementApp/DTO/UserReadOnlyDTO.cs
@@ -5,11 +5,11 @@ namespace LibraryManagementApp.DTO
     public record UserReadOnlyDTO
     {
         public int Id { get; set; }
-        public string Username { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string Firstname { get; set; } = null!;
-        public string Lastname { get; set; } = null!;
-        public string UserRole { get; set; } = null!;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public string UserRole { get; set; } = string.Empty;
 
     }
 }
