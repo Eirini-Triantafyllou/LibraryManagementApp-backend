80829d0c67e12c96ae4e8795fdfbae69a4b1e4eb
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-08 21:25:04 +0200

    feat: create WishlistItemDTO

---
+++ LibraryManagementApp/DTO/WishlistItemDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagementApp.DTO
+{
+    public record WishlistItemDTO
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string Title { get; set; } = null!;
+        public string ISBN { get; set; } = null!;
+        public string AuthorName { get; set; } = null!;
+        public DateTime AddedAt { get; set; }
+
+    }
+}
