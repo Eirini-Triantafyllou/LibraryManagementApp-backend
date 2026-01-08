30387d4d6da523c78e2204b63e5b72d79e083d09
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-08 20:10:29 +0200

    feat: create wishlist entity

---
+++ LibraryManagementApp/Data/Wishlist.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagementApp.Data
+{
+    public class Wishlist : BaseEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public virtual User User { get; set; } = null!;
+        public virtual Book Book { get; set; } = null!;
+        public DateTime AddedAt { get; set; }
+
+    }
+}
