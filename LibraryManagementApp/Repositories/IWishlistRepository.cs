c01442f92b36eb7548a17142988701489cbb488e
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-09 18:17:32 +0200

    feat: create wishlist repository

---
+++ LibraryManagementApp/Repositories/IWishlistRepository.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagementApp.DTO;
+
+namespace LibraryManagementApp.Repositories
+{
+    public interface IWishlistRepository
+    {
+        Task<IEnumerable<WishlistItemDTO>> GetUserWishlistAsync(int userId);
+        Task<bool> IsBookInWishlistAsync(int userId, int bookId);
+        Task AddToWishlistAsync(int userId, int bookId);
+        Task RemoveFromWishlistAsync(int userId, int bookId);
+
+    }
+}
---
+++ LibraryManagementApp/Repositories/WishlistRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LibraryManagementApp.Data;
+using LibraryManagementApp.DTO;
+using System.Linq.Expressions;
+
+namespace LibraryManagementApp.Repositories
+{
+    public class WishlistRepository : BaseRepository<Wishlist>, IWishlistRepository
+    {
+        public WishlistRepository(LibraryAppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<bool> IsBookInWishlistAsync(int userId, int bookId)
+        {
+            return await context.Wishlists
+                .AnyAsync(w => w.UserId == userId && w.BookId == bookId);
+        }
+
+        public async Task AddToWishlistAsync(int userId, int bookId)
+        {
+            var wishlistItem = new Wishlist
+            {
+                UserId = userId,
+                BookId = bookId,
+                AddedAt = DateTime.UtcNow
+            };
+            await AddAsync(wishlistItem);
+        }
+
+        public async Task<IEnumerable<WishlistItemDTO>> GetUserWishlistAsync(int userId)
+        {
+            return await context.Wishlists
+                .Where(w => w.UserId == userId)
+                .Include(w => w.Book)
+                    .ThenInclude(b => b.Author)
+                .Select(w => new WishlistItemDTO
+                {
+                    Id = w.Id,
+                    BookId = w.BookId,
+                    Title = w.Book.Title,
+                    ISBN = w.Book.ISBN,
+                    AuthorName = w.Book.Author != null 
+                            ? $"{w.Book.Author.Firstname} {w.Book.Author.Lastname}" 
+                            : "Unknown",
+                    AddedAt = w.AddedAt
+                })
+                .ToListAsync();
+        }
+        public async Task RemoveFromWishlistAsync(int userId, int bookId)
+        {
+            var wishlistItem = await context.Wishlists
+               .FirstOrDefaultAsync(w => w.UserId == userId && w.BookId == bookId);
+
+            if (wishlistItem == null) return;
+
+            await DeleteAsync(wishlistItem!.Id);
+        }
+    }
+}
