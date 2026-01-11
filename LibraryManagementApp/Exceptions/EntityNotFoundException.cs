a3767d33be901a07cf9ad9a6aeaf694a4f28dcd9
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-11 19:42:29 +0200

    Update EntityNotFoundException

--- LibraryManagementApp/Exceptions/EntityNotFoundException.cs
+++ LibraryManagementApp/Exceptions/EntityNotFoundException.cs
@@ -3,6 +3,7 @@
     public class EntityNotFoundException : AppException
     {
         private static readonly string DEFAULT_CODE = "entity_not_found";
+
         public EntityNotFoundException(string code, string message)
             : base(code + DEFAULT_CODE, message)
         {
