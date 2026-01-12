f86abf32f6bf6954f42bae394fc42b9c88aae280
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-12 23:19:51 +0200

    chore: remove unnecessary comments

--- LibraryManagementApp/DTO/UserLoginDTO.cs
+++ LibraryManagementApp/DTO/UserLoginDTO.cs
@@ -4,9 +4,6 @@ namespace LibraryManagementApp.DTO
 {
     public record UserLoginDTO
     {
-        //[StringLength(50, MinimumLength = 2, ErrorMessage = "Username must be between 2 and 50 characters.")]
-        //public string? Username { get; set; } 
-
         [Required(ErrorMessage = "Email is required")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Invalid email format")]
         public string? Email { get; set; } 
