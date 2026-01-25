2dd0da1711faa2c1a5385729c1e4959f5fe2561e
E. Triantafyllou <irenetriantafyllou@msn.com>
2026-01-25 21:31:46 +0200

    fix: change fullName to firstname and lastname

--- LibraryManagementApp/Data/Librarian.cs
+++ LibraryManagementApp/Data/Librarian.cs
@@ -3,11 +3,13 @@
     public class Librarian : BaseEntity
     {
         public int Id { get; set; }
-        public string FullName { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
-        public DateTime HireDate { get; set; }
-        public bool IsActive { get; set; }
+        public string Address { get; set; } = null!;
+        public DateTime? HireDate { get; set; }
+        public bool? IsActive { get; set; }
         public int UserId { get; set; }
         public virtual User User { get; set; } = null!;
         public virtual ICollection<Reader> Readers { get; set; } = new HashSet<Reader>();
--- LibraryManagementApp/Data/Reader.cs
+++ LibraryManagementApp/Data/Reader.cs
@@ -3,13 +3,14 @@
     public class Reader : BaseEntity
     {
         public int Id { get; set; }
-        public string FullName { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
         public string Address { get; set; } = null!;
-        public DateTime MembershipDate { get; set; }
-        public bool IsActive { get; set;  } 
-        public int BooksBorrowedCount { get; set; }
+        public DateTime? MembershipDate { get; set; }
+        public bool? IsActive { get; set;  } 
+        public int? BooksBorrowedCount { get; set; }
         public int UserId { get; set; }
         public virtual User User { get; set; } = null!;
         public virtual ICollection<Book> Books { get; set; } = new HashSet<Book>();
